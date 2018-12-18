78e0f4c174a38f1616ae8134544d4e13b425cb4c
siraben <bensiraphob@gmail.com>
2018-12-18 19:52:13 +0700

    Change `make-inst`, add instructions and boot-prog.
    New instructions (partial indicated):
    - xor (partial)
    - out
    - dec (partial)

--- assembler3.scm
+++ assembler3.scm
@@ -10,7 +10,8 @@
   '((af . #b11)
     (bc . #b00)
     (de . #b01)
-    (hl . #b10)))
+    (hl . #b10)
+    (sp . #b11)))
 
 (define push-pop-index-regs
   '((ix . #b11011101)
@@ -58,24 +59,29 @@
           opcode))
 
 (define-record-type <inst>
-  (make-inst cycles length generator)
+  (make-inst-rec cycles length generator)
   inst?
   (cycles inst-cycles)
   (length inst-length)
   (generator inst-generator))
+
+(define-syntax make-inst
+  (syntax-rules ()
+    ((_ cycles length generator)
+     (make-inst-rec cycles length (delay generator)))))
 
 (define (gen-inst inst)
   (force (inst-generator inst)))
 
 (define (assemble-push reg)
   (if (index-reg? reg)
-      (make-inst 15 2 (delay `(,(lookup reg push-pop-index-regs) #b11100101)))
-      (make-inst 11 1 (delay `(,(make-opcode (lookup reg 16-bit-regs) 4 #b11000101))))))
+      (make-inst 15 2 `(,(lookup reg push-pop-index-regs) #b11100101))
+      (make-inst 11 1 `(,(make-opcode (lookup reg 16-bit-regs) 4 #b11000101)))))
 
 (define (assemble-pop reg)
   (if (index-reg? reg)
-      (make-inst 14 2 (delay `(,(lookup reg push-pop-index-regs) #b11100001)))
-      (make-inst 10 1 (delay `(,(make-opcode (lookup reg 16-bit-regs) 4 #b11000001))))))
+      (make-inst 14 2  `(,(lookup reg push-pop-index-regs) #b11100001))
+      (make-inst 10 1 `(,(make-opcode (lookup reg 16-bit-regs) 4 #b11000001)))))
 
 (define (unsigned-nat? x)
   (and (integer? x) (>= x 0)))
@@ -107,24 +113,22 @@
 (define (assemble-ld-reg8-reg8 dest src)
   (make-inst (if (eqv? dest '(hl)) 7 4)
              1
-             (delay
-               `(,(make-opcode (lookup src ld-regs)
-                               0
-                               (make-opcode (lookup dest ld-regs)
-                                            3
-                                            #b01000000))))))
+             `(,(make-opcode (lookup src ld-regs)
+                             0
+                             (make-opcode (lookup dest ld-regs)
+                                          3
+                                          #b01000000)))))
 (define (assemble-ld-reg8-imm8 reg8 imm8)
   (make-inst (if (eqv? reg8 '(hl)) 10 7)
              2
-             (delay
-               `(,(make-opcode (lookup reg8 ld-regs) 3 #b00000110) ,imm8))))
+             `(,(make-opcode (lookup reg8 ld-regs) 3 #b00000110) ,imm8)))
 
 (define (assemble-ld-hl-iimm16 iimm16)
   (make-inst 16
              3
-             (delay `(#b00101010
-                      ,(lsb iimm16)
-                      ,(msb iimm16)))))
+             `(#b00101010
+               ,(lsb iimm16)
+               ,(msb iimm16))))
 
 (define ld-iregs
   '((bc . #b0) (de . #b1)))
@@ -132,8 +136,7 @@
 (define (assemble-ld-a-ireg16 reg)
   (make-inst 14
              3
-             (delay
-               `(,(make-opcode (lookup reg ld-iregs) 4 #b00001010)))))
+             `(,(make-opcode (lookup reg ld-iregs) 4 #b00001010))))
 
 ;; Least significant byte.
 (define (lsb n)
@@ -152,15 +155,15 @@
 (define (assemble-ld-reg16-imm16 reg16 imm16)
   (make-inst 10
              3
-             (delay (let ((imm16 (resolve-label imm16)))
-                      `(,(make-opcode (lookup reg16 16-bit-regs) 4 #b00000001)
-                        ,(lsb imm16)
-                        ,(msb imm16))))))
+             (let ((imm16 (resolve-label imm16)))
+               `(,(make-opcode (lookup reg16 16-bit-regs) 4 #b00000001)
+                 ,(lsb imm16)
+                 ,(msb imm16)))))
 
 (define (assemble-ld-ireg16-a reg16)
   (make-inst 7
              1
-             (delay `(,(make-opcode (lookup reg16 16-bit-regs) 4 #b00000010)))))
+             `(,(make-opcode (lookup reg16 16-bit-regs) 4 #b00000010))))
 
 (define (assemble-ld args)
   (match args
@@ -176,7 +179,8 @@
      (assemble-ld-reg16-imm16 a b))
     (('hl ((? 16-bit-imm-or-label? b)))
      (assemble-ld-hl-iimm16 b))
-    )
+    (_
+     (error (format #f "Invalid operands to ld: ~a" args))))
   )
 
 (define (simple-op? op) (lookup op simple-ops))
@@ -215,7 +219,7 @@
 (define (assemble-simple a)
   (let ((res (lookup a simple-ops)))
     (if res
-        (make-inst (car res) (cadr res) (delay (caddr res)))
+        (make-inst (car res) (cadr res) (caddr res))
         (error (format #f "Operation not found: ~a" a)))))
 
 (define (add-label! name)
@@ -236,18 +240,18 @@
 (define (assemble-cond-jp cond imm16)
   (make-inst 10
              3
-             (delay (let ((imm16 (resolve-label imm16)))
-                      `(,(make-opcode (lookup cond condition-codes) 3 #b11000010)
-                        ,(lsb imm16)
-                        ,(msb imm16))))))
+             (let ((imm16 (resolve-label imm16)))
+               `(,(make-opcode (lookup cond condition-codes) 3 #b11000010)
+                 ,(lsb imm16)
+                 ,(msb imm16)))))
 
 (define (assemble-uncond-jp imm16)
   (make-inst 10
              3
-             (delay (let ((imm16 (resolve-label imm16)))
-                      `(#b11000011
-                        ,(lsb imm16)
-                        ,(msb imm16))))))
+             (let ((imm16 (resolve-label imm16)))
+               `(#b11000011
+                 ,(lsb imm16)
+                 ,(msb imm16)))))
 
 (define (assemble-jp args)
   (match args
@@ -260,18 +264,18 @@
 (define (assemble-cond-call cond imm16)
   (make-inst 13.5
              3
-             (delay (let ((imm16 (resolve-label imm16)))
-                      `(,(make-opcode (lookup cond condition-codes) 3 #b11000100)
-                        ,(lsb imm16)
-                        ,(msb imm16))))))
+             (let ((imm16 (resolve-label imm16)))
+               `(,(make-opcode (lookup cond condition-codes) 3 #b11000100)
+                 ,(lsb imm16)
+                 ,(msb imm16)))))
 
 (define (assemble-uncond-call imm16)
   (make-inst 17
              3
-             (delay (let ((imm16 (resolve-label imm16)))
-                      `(#b11001101
-                        ,(lsb imm16)
-                        ,(msb imm16))))))
+             (let ((imm16 (resolve-label imm16)))
+               `(#b11001101
+                 ,(lsb imm16)
+                 ,(msb imm16)))))
 
 (define (assemble-call args)
   (match args
@@ -283,15 +287,60 @@
 (define (assemble-dw word-list)
   (make-inst 0
              (ash (length word-list) 1)
-             (delay (flatten (map
-                              (lambda (x)
-                                (list (lsb x) (msb x)))
-                              word-list)))))
+             (flatten (map
+                       (lambda (x)
+                         (list (lsb x) (msb x)))
+                       word-list))))
 
 (define (assemble-db byte-list)
   (make-inst 0
              (length byte-list)
-             (delay byte-list)))
+             byte-list))
+
+(define (assemble-out-iimm8-a port)
+  (make-inst 11
+             2
+             `(#b11010011
+               ,port)))
+
+(define (assemble-out-c-reg reg)
+  (make-inst 12
+             2
+             `(#b11101011
+               ,(make-opcode (lookup reg ld-regs) 3 #b01000001))))
+
+(define (assemble-out arg)
+  (match arg
+    ((((? 8-bit-imm? p)) 'a)
+     (assemble-out-iimm8-a p))
+    (`((c) ,(? 8-bit-reg? r))
+     (assemble-out-c-reg r))
+    (_
+     (error (format #f "Invalid operands to out: ~a" arg)))))
+
+(define (assemble-xor-8-bit-reg a)
+  (make-inst (if (eqv? a '(hl)) 7 4)
+             1
+             `(,(make-opcode (lookup a ld-regs) 0 #b10101000))))
+
+(define (assemble-xor arg)
+  (match arg
+    ((? 8-bit-reg? a)
+     (assemble-xor-8-bit-reg a))
+    (_
+     (error (format #f "Invalid operands to xor: ~a" arg)))))
+
+(define (assemble-dec-8-bit-reg a)
+  (make-inst (if (eqv? a '(hl)) 11 4)
+             1
+             `(,(make-opcode (lookup a ld-regs) 3 #b00000101))))
+
+(define (assemble-dec arg)
+  (match arg
+    ((? 8-bit-reg? a)
+     (assemble-dec-8-bit-reg a))
+    (_
+     (error (format #f "Invalid operands to dec: ~a" arg)))))
 
 (define (assemble-expr expr)
   (match expr
@@ -315,6 +364,12 @@
      (assemble-db arg))
     (`(dw ,arg)
      (assemble-dw arg))
+    (`(out ,dest ,src)
+     (assemble-out `(,dest ,src)))
+    (`(xor ,arg)
+     (assemble-xor arg))
+    (`(dec ,arg)
+     (assemble-dec arg))
     (_
      (error (format #f "Unknown expression: ~s\n" expr))))
   )
@@ -362,14 +417,8 @@
 (define (assemble-to-binary prog)
   (map num->binary (flatten (assemble-prog prog))))
 
-(define (assemble-to-binary2 prog)
-  (map num->binary (flatten (assemble-prog2 prog))))
-
 (define (assemble-to-file prog filename)
   (write-bytevector-to-file (u8-list->bytevector (flatten (assemble-prog prog))) filename))
-
-(define (assemble-to-file2 prog filename)
-  (write-bytevector-to-file (u8-list->bytevector (flatten (assemble-prog2 prog))) filename))
 
 ;; (assemble-to-file sample-prog "out.bin")
 
@@ -395,5 +444,56 @@
   ;; Force the code generating thunks.  All labels should be resolved by now.
   (map-in-order gen-inst insts))
 
-(define (assemble-prog2 prog)
+(define (assemble-prog prog)
   (pass2 (pass1 prog)))
+
+;; A more extensive test program for the assembler.
+(define boot-prog
+  `((jp boot)
+    ;; Notice because this is Scheme we have access to all the
+    ;; wonderful magic of quasi-quotation.
+    (db ,(map char->integer (string->list "SK")))
+    (db (0 0 #b00110011))
+    ,@(make-list 48 '(ret))
+    (jp sys-interrupt)
+    (db ,(make-list 24 0))
+    (jp boot)
+    (db (#xff #xa5 #xff))
+    (label boot)
+    (label shutdown)
+    (di)
+    (ld a 6)
+    (out (4) a)
+    (ld a #x81)
+    (out (7) a)
+    (ld sp 0)
+    (call sleep)
+    (label restart)
+    (label reboot)
+    (di)
+    (ld sp 0)
+    (ld a 6)
+    (out (4) a)
+    (ld a #x81)
+    (out (7) a)
+    (ld a 3)
+    (out (#xe) a)
+    (xor a)
+    (out (#xf) a)
+    (call unlock-flash)
+    (xor a)
+    (out (#x25) a)
+    (dec a)
+    (out (#x26) a)
+    (out (#x23) a)
+    (out (#x22) a)
+    (call lock-flash)
+    (ld a 1)
+    (out (#x20) a)
+    (label unlock-flash)
+    (ret)
+    (label lock-flash)
+    (ret)
+    (label sleep)
+    (ret)
+    (label sys-interrupt)))
