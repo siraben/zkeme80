d7014497bf2e6fb89a069b752ac998cbf95fa79f
Ben Siraphob <bensiraphob@gmail.com>
2021-06-07 14:08:12 +0700

    Use flake-compat

--- default.nix
+++ default.nix
@@ -1,18 +1,7 @@
-{ sources ? import nix/sources.nix
-, pkgs ? import sources.nixpkgs { }
-}:
-with pkgs;
-
-rec {
-  zkeme80 = runCommand "zkeme80.rom" { buildInputs = [ guile ]; } ''
-    cp -r ${./.}/src/* .
-    chmod -R +w .
-    echo '(begin (load "zkeme80.scm") (make-rom "zkeme80.rom"))' | guile
-    mkdir $out
-    cp zkeme80.rom $out/
-  '';
-  runit = writeScript "runit" ''
-    #!/usr/bin/env sh
-    ${tilem}/bin/tilem2 -r ${zkeme80}/zkeme80.rom
-  '';
-}
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
+) {
+  src =  ./.;
+}).defaultNix
