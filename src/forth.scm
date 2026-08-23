94a27ec02d91032748c43f4dca82e6a197373803
Ben Siraphob <bensiraphob@gmail.com>
2026-08-22 22:47:21 -0700

    sources: migrate ((ex ...)) to single-paren (ex ...)
    Follow-up to 2b11865 which taught assemble-expr to accept instruction
    forms that are simple-op keys without an extra layer of parens.
    Migrate all 20 call sites in display/forth/interrupt/text; ROM is
    byte-identical before and after (md5 63a766574ca332788d334c3ed00777b9).

--- src/forth.scm
+++ src/forth.scm
@@ -248,7 +248,7 @@
 
     ,@(defcode "ROT" 0 'rot)
     (pop hl)
-    ((ex (sp) hl))
+    (ex (sp) hl)
     (push bc)
     ,@hl-to-bc
     ,@next
@@ -257,7 +257,7 @@
     (ld h b)
     (ld l c)
     (pop bc)
-    ((ex (sp) hl))
+    (ex (sp) hl)
     (push hl)
     ,@next
 
@@ -279,7 +279,7 @@
     (ld d b)
     (ld e c)
     (pop bc)
-    ((ex (sp) hl))
+    (ex (sp) hl)
     (push de)
     (push hl)
     (ld de (var-temp-cell))
@@ -564,7 +564,7 @@
     (ld d a)
     (dec bc)
     (xor a)
-    ((ex de hl))
+    (ex de hl)
     (sbc hl de)
 
     (ld a l)
@@ -911,14 +911,14 @@
     ,@next
 
     ,@(defcode "BRANCH" 0 'branch)
-    ((ex de hl))
+    (ex de hl)
     (ld e (hl))
     (inc hl)
     (ld d (hl))
     (dec hl)
 
     (add hl de)
-    ((ex de hl))
+    (ex de hl)
     ,@next
 
     ,@(defcode "0BRANCH" 0 '0branch)
@@ -1226,7 +1226,7 @@
     ,@hl-to-bc
     (add hl de)
     (inc hl)
-    ((ex de hl))
+    (ex de hl)
     ,@next
 
     ,@(defword "S\"" immediate 's-quote)
@@ -1291,7 +1291,7 @@
     (dec de)
     (dec de)
     (pop hl)
-    ((ex de hl))
+    (ex de hl)
     ,@hl-to-bc
     ,@next
 
@@ -1316,7 +1316,7 @@
     (inc hl)
     (inc hl)
     (inc hl)
-    ((ex de hl))
+    (ex de hl)
     (pop hl)
     (jp find-loop)
 
@@ -1382,7 +1382,7 @@
     (ld (hl) b)
     (inc hl)
     (ld de var-dp)
-    ((ex de hl))
+    (ex de hl)
     (ld (hl) e)
     (inc hl)
     (ld (hl) d)
@@ -1400,7 +1400,7 @@
     (ld (hl) c)
     (inc hl)
     (ld de var-dp)
-    ((ex de hl))
+    (ex de hl)
     (ld (hl) e)
     (inc hl)
     (ld (hl) d)
@@ -1474,7 +1474,7 @@
     (ld (hl) a)
     (inc hl)
 
-    ((ex de hl))
+    (ex de hl)
     (pop hl)
     (ld b 0)
     (ldir)
@@ -1895,7 +1895,7 @@
     (dw (lit ddd exit))
 
     (label ddd)
-    ((ex af afs))
+    (ex af afs)
     (exx)
 
     (ld iy #x8100)
@@ -1917,7 +1917,7 @@
     (call rect-xor)
     (call fast-copy)
 
-    ((ex af afs))
+    (ex af afs)
     (exx)
 
 
