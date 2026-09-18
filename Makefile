34de6bacc8d370d2b0bc10a9070d298e2b8c37f6
Ben Siraphob <bensiraphob@gmail.com>
2026-09-18 14:05:32 -0700

    Fix build entry points and include shell and tests in upgrades

--- Makefile
+++ Makefile
@@ -1,11 +1,19 @@
-build:
-	cd src && make build && cp zkeme80.rom zkeme80.ram-labelmap.json ../
+.PHONY: build all upgrade test-build test-emulator
 
-all:
-	cd src && make build && cp zkeme80.rom zkeme80.ram-labelmap.json ../
+build:
+	$(MAKE) -C src build
+	cp src/zkeme80.rom src/zkeme80.ram-labelmap.json .
+
+all: build
 	tilem2 -r zkeme80.rom
 
 upgrade:
-	cd src && make build
-	mktiupgrade -k 0A.key --device TI-84+ zkeme80.rom zkeme80.8xu 00 01 02 03 3C
-	cp zkeme80.8xu ../
+	$(MAKE) -C src upgrade
+	cp src/zkeme80.8xu .
+
+test-build:
+	python3 tests/test-build.py
+
+# Requires a TilEm build with --headless and --macro; set TILEM to its path.
+test-emulator: build
+	python3 tests/master-kernel.py
--- build.scm
+++ build.scm
@@ -1,3 +1,14 @@
-(add-to-load-path "src")
-(load (%search-load-path "zkeme80.scm"))
-(make-rom+map "zkeme80.rom" "zkeme80.ram-labelmap.json")
+(use-modules (ice-9 format))
+
+;; Source includes are relative to src; outputs belong to the caller's directory.
+(let ((source-directory
+       (string-append (dirname (canonicalize-path (current-filename))) "/src"))
+      (output-directory (getcwd)))
+  (dynamic-wind
+    (lambda () (chdir source-directory))
+    (lambda ()
+      (load (string-append source-directory "/zkeme80.scm"))
+      ((module-ref (current-module) 'make-rom+map)
+       (string-append output-directory "/zkeme80.rom")
+       (string-append output-directory "/zkeme80.ram-labelmap.json")))
+    (lambda () (chdir output-directory))))
